a96fa6ddb25fd72f1e5bb364f54422baa252315d
K4Jo73 <k4jo73@gmail.com>
2021-10-23 19:33:40 +0100

    moved practice code into dummy functions to tidy up

--- atm_app_start.py
+++ atm_app_start.py
@@ -5,69 +5,31 @@ import atm_taskdb as taskdb
 def main():
     audit.setup_logging("./logs/")
     audit.print_heading('Automation Task Manager')
-    audit.print_subheading("Task Status List")
-    statusRecords = taskdb.getRecords("TaskStatus")
 
-    for statusrec in statusRecords:
-        print(statusrec[0], statusrec[1])
-
-
-    audit.print_line()
-    audit.print_subheading("Task Type List")
-    typeRecords = taskdb.getRecords("TaskType")
-
-    for typerec in typeRecords:
-        print(typerec[0],typerec[1])
-
-
-    audit.print_line()
-    audit.print_subheading("Adding New Task")
-    taskTypes = taskdb.getRecords(tablename="TaskType", criteria="type_name = 'DoThing'", maxrecords=1)
-
-    for typerec in taskTypes:
-        print(typerec)
-
-
-    audit.print_line()
-    audit.print_subheading("Task Queue List")
-    QueueRecords = taskdb.getRecords("TaskQueuex","",4)
-
-    for queuerec in QueueRecords:
-        print(queuerec)
-
-
-    audit.print_line()
-
+    # getPickList("Status")
+    # getPickList("Type")
     # taskdb.getTableColumns("task_queue")
-
-    #ParamList
-    param1 = ("source_ref","test record")
-    param2 = ("source_id","abcde-12345-abcde-98765-ZYXWV")
-    param3 = ("note","change that value on users account")
-    param4 = ("activity_param01","John.Smith")
-    param5 = ("activity_param02","thing to be changed")
-    paramslist = []
-    print(paramslist)
-    paramslist.append(param1)
-    paramslist.append(param2)
-    paramslist.append(param3)
-    paramslist.append(param4)
-    paramslist.append(param5)
-
-    audit.print_line()
-    addtaskresult = taskdb.addTask("DoThing",paramslist)
-    audit.logging.info("Added Task ID - " + str(addtaskresult))
+    # saveDummyEmptyTask()
+    # saveDummyDetailedTask()
+    # getSomeTasks(10)
 
 
-    audit.print_subheading("Task Queue Record Detail")
-    QueueRecords = taskdb.getRecords("TaskQueue", "task_id = " + str(addtaskresult))
+
+
+
+
+
+def getSomeTasks(howmany):
+    audit.print_subheading("Task Queue List Sample")
+    QueueRecords = taskdb.getRecords("TaskQueue","",howmany)
 
     for queuerec in QueueRecords:
         print(queuerec)
 
+    return QueueRecords
 
 
-
+def saveDummyEmptyTask():
     audit.print_line()
     addtaskresult = taskdb.addTask("DoThing")
     audit.logging.info("Added Task ID - " + str(addtaskresult))
@@ -77,6 +39,49 @@ def main():
 
     for queuerec in QueueRecords:
         print(queuerec)
+
+    return QueueRecords
+
+
+def saveDummyDetailedTask():
+    audit.print_line()
+    audit.print_subheading("Adding New Task")
+
+    #ParamList
+    param1 = ("source_ref","test record")
+    param2 = ("source_id","abcde-12345-abcde-98765-ZYXWV")
+    param3 = ("note","change that value on users account")
+    param4 = ("activity_param01","John.Smith")
+    param5 = ("activity_param02","thing to be changed")
+    paramslist = []
+    paramslist.append(param1)
+    paramslist.append(param2)
+    paramslist.append(param3)
+    paramslist.append(param4)
+    paramslist.append(param5)
+
+    addtaskresult = taskdb.addTask("DoThing",paramslist)
+    audit.logging.info("Added Task ID - " + str(addtaskresult))
+
+    audit.print_subheading("Saved Task Queue Record Detail")
+    QueueRecords = taskdb.getRecords("TaskQueue", "task_id = " + str(addtaskresult))
+
+    for queuerec in QueueRecords:
+        print(queuerec)
+
+    return QueueRecords
+
+
+def getPickList(listtype):
+    audit.print_subheading("Task "+ listtype + " List")
+    Records = taskdb.getRecords("Task" + listtype)
+
+    for rec in Records:
+        print(rec[0], rec[1])
+
+    audit.print_line()
+   
+    return Records
 
 
 
